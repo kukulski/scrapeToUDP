87f522dfecb30ccbf10b7c0419bd20ee689a0128
kukulski <kukulski@adobe.com>
2012-10-30 16:51:55 -0700

    command line tool to scrape pixels from screen out to a UDP port

--- scrapeToUDP/main.cpp
+++ scrapeToUDP/main.cpp
@@ -8,11 +8,32 @@
 
 #include <iostream>
 
+void printUsage(const char*name);
+
 int main(int argc, const char * argv[])
 {
-
+    if(argc == 1)
+        printUsage(argv[0])
+    int i = 1;
+    
+    while(i < argc)
+        i = eatArgs(argv, i);
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
 
+void printUsage(const char*name) {
+    
+    printf("usage: %s <x> <y> <width> <height> [scale] \n",argv[0]);
+    
+}
+
+
+NSColor *MyColorAtScreenCoordinate(CGDirectDisplayID displayID, NSInteger x, NSInteger y) {
+    CGImageRef image = CGDisplayCreateImageForRect(displayID, CGRectMake(x, y, 1, 1));
+    NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithCGImage:image];
+    CGImageRelease(image);
+    NSColor *color = [bitmap colorAtX:0 y:0];
+    [bitmap release];
+}

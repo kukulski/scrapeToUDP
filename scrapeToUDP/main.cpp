c4f8807eeef65ee88a185b6e5b83c4cae72af6ad
kukulski <kukulski@adobe.com>
2012-10-30 16:30:30 -0700

    Initial Commit

---
+++ scrapeToUDP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  scrapeToUDP
+//
+//  Created by Timothy Kukulski on 10/30/12.
+//  Copyright (c) 2012 Adobe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
